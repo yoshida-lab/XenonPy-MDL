5edc8bb8e89c070eb0a6b96a738b9e8ac10a0c7f
TsumiNa <liu.chang.1865@gmail.com>
2021-04-05 04:42:11 +0000

    update packages except that around prisma

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   poweredByHeader: false,
   reactStrictMode: true,
+  future: {
+    webpack5: true
+  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if your project has type errors.
